5cc860bb89b0ae1359735f8285327db8fcf78f09
wfercanas <fernando.canas@outlook.com>
2022-06-17 23:36:58 -0500

    refactor/ social media width and footer height

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 
-const StyledFooter = styled.footer`
-  position: sticky;
-  bottom: 0px;
-`;
+const StyledFooter = styled.footer``;
 
 const StyledFooterContainer = styled.div`
   margin: 0 auto;
@@ -15,16 +12,18 @@ const StyledSocialMediaContainer = styled.div`
   background-color: var(--n400);
   color: var(--n10);
   border-top-right-radius: 16px;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 const StyledSocialMediaList = styled.ul`
-  max-width: 500px;
+  width: 500px;
   height: 100%;
   list-style: none;
   padding: 0;
   margin: 0;
   display: flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
   align-items: center;
   gap: 24px;
 `;
@@ -41,7 +40,7 @@ const StyledSocialMediaIcon = styled.img`
 `;
 
 const StyledNameContainer = styled.div`
-  height: 72px;
+  height: 100px;
   display: flex;
   align-items: center;
 `;
@@ -50,6 +49,8 @@ const StyledName = styled.p`
   text-align: end;
   width: 500px;
   color: var(--n900);
+  font-size: var(--fs-h600);
+  cursor: default;
 `;
 
 export {
