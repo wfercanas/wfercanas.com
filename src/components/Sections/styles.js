38cf0b3a54407623e5fa0fcd42739afa39d9ee0a
wfercanas <fernando.canas@outlook.com>
2022-06-17 23:04:49 -0500

    fix/ increase margin-bottom and add padding-bottom

--- src/components/Sections/styles.js
+++ src/components/Sections/styles.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const StyledSection = styled.section`
   max-width: 1000px;
-  margin: 0 auto 80px;
+  margin: 0 auto 100px;
+  padding-bottom: 80px;
   border-bottom: ${({ divider }) => (divider ? "2px solid var(--n300)" : "0")};
 `;
 
