04196aab8738ee01897c4276e9f1244beedd42a8
wfercanas <fernando.canas@outlook.com>
2022-06-17 22:02:33 -0500

    add/ heroku and cloudinary apis

---
+++ src/utils/api.js
@@ -0,0 +1,6 @@
+const HK_API = "https://backend-wfercanas.herokuapp.com/api";
+const HK_ARTICLES_ENDPOINT = "/articles";
+
+const CDN_API = "https://res.cloudinary.com/wfercanas/image/upload";
+
+export { HK_API, HK_ARTICLES_ENDPOINT, CDN_API };
