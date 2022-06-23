aefd2b33053c976d2d9cf469108c0333ffcb3438
wfercanas <fernando.canas@outlook.com>
2022-06-22 22:38:19 -0500

    refactor/ use coverURL instead of local image

--- src/components/Cards/Cards.stories.js
+++ src/components/Cards/Cards.stories.js
@@ -1,6 +1,4 @@
 import { ArticleCard, HeroCard } from ".";
-import mediumImage from "../../assets/images/sticky-notes-medium.jpg";
-import smallImage from "../../assets/images/sticky-notes-small.jpg";
 
 export default {
   title: "Components / Cards",
@@ -14,7 +12,7 @@ export const Hero = (args) => {
 Hero.args = {
   title: "Be a startup again when trying to innovate with a new product",
   date: "10/06/2022",
-  image: mediumImage,
+  image: "/v1655517912/wfercanas.com/sticky-notes_wpowei.jpg",
   alt: "A group working together on a table and a whiteboard with sticky notes",
   figcaption: "Photo by Jason Goodman on Unsplash",
   showCaption: true,
@@ -27,6 +25,6 @@ export const Article = (args) => {
 Article.args = {
   title: "Listen and lead as basis for creating awesome products",
   date: "01/06/2022",
-  image: smallImage,
+  image: "/v1655517912/wfercanas.com/sticky-notes_wpowei.jpg",
   alt: "Many cups of coffee on top of a table where people are working with papers",
 };
