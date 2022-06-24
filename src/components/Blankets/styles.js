fcabfc1d815f4bfce895b19531757468126e7ece
wfercanas <fernando.canas@outlook.com>
2022-06-24 16:10:40 -0500

    refactor/ position fixed and darker background

--- src/components/Blankets/styles.js
+++ src/components/Blankets/styles.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 const StyledBlanket = styled.div`
-  position: absolute;
+  position: fixed;
   left: 0;
   right: 0;
   top: 0;
   bottom: 0;
-  background-color: var(--n600a);
+  background-color: var(--n800a);
 `;
 
 export { StyledBlanket };
