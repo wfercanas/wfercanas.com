fde63661d68a9b5899ff6da505b4fa5f60d19540
wfercanas <fernando.canas@outlook.com>
2022-06-23 23:42:16 -0500

    add/ Router with /, /articles and /projects paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Navbar } from "./components/Navbar";
 import { HomeContainer } from "./containers/pages/HomeContainer";
+import { ArticlesPage } from "./pages/Articles";
+import { ProjectsPage } from "./pages/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <HomeContainer />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeContainer />} />
+        <Route path="/articles" element={<ArticlesPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
