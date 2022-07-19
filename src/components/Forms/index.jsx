94d172ef1afa701158e9c177f222f694ef208491
wfercanas <fernando.canas@outlook.com>
2022-07-19 15:47:39 -0500

    refactor/ decouple logic and interface in 2 files

--- src/components/Forms/index.jsx
+++ src/components/Forms/index.jsx
@@ -1,12 +1,6 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { EMAIL_API } from "../../api/endpoints";
-
-import { SubmitButton } from "../Buttons";
-import { ContactInput } from "../Inputs";
-import { ContactTextarea } from "../Textareas";
-
-import { StyledContactForm } from "./styles";
+import { ContactFormUI } from "./interface";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -44,33 +38,11 @@ const ContactForm = () => {
   };
 
   return (
-    <StyledContactForm onSubmit={handleSubmit}>
-      <ContactInput
-        id="name"
-        type="text"
-        value={formData.name}
-        placeholder="Name"
-        required={true}
-        handleChange={handleChange}
-      />
-      <ContactInput
-        id="email"
-        type="email"
-        value={formData.email}
-        placeholder="Email"
-        required={true}
-        handleChange={handleChange}
-      />
-      <ContactTextarea
-        id="message"
-        placeholder="Message"
-        value={formData.message}
-        required={true}
-        handleChange={handleChange}
-        maxlength={200}
-      />
-      <SubmitButton label="Send" />
-    </StyledContactForm>
+    <ContactFormUI
+      handleSubmit={handleSubmit}
+      handleChange={handleChange}
+      formData={formData}
+    />
   );
 };
 
---
+++ src/components/Forms/interface.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { SubmitButton } from "../Buttons";
+import { ContactInput } from "../Inputs";
+import { ContactTextarea } from "../Textareas";
+
+import { StyledContactForm } from "./styles";
+
+const ContactFormUI = ({ handleSubmit, handleChange, formData }) => {
+  <StyledContactForm onSubmit={handleSubmit}>
+    <ContactInput
+      id="name"
+      type="text"
+      value={formData.name}
+      placeholder="Name"
+      required={true}
+      handleChange={handleChange}
+    />
+    <ContactInput
+      id="email"
+      type="email"
+      value={formData.email}
+      placeholder="Email"
+      required={true}
+      handleChange={handleChange}
+    />
+    <ContactTextarea
+      id="message"
+      placeholder="Message"
+      value={formData.message}
+      required={true}
+      handleChange={handleChange}
+      maxlength={200}
+    />
+    <SubmitButton label="Send" />
+  </StyledContactForm>;
+};
+
+export { ContactFormUI };
