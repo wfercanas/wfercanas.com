4e902159bf9bd6d10648f57a510a6dd80af57d76
wfercanas <fernando.canas@outlook.com>
2022-07-20 15:56:56 -0500

    add/ submit button controls loading, success and failure states

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 import { StyledSubmitButton } from "./styles";
+import { Spinner } from "../Spinners";
 
-const SubmitButton = ({ label = "Submit" }) => {
-  return <StyledSubmitButton>{label}</StyledSubmitButton>;
+const SubmitButton = ({ label = "Submit", status = "Active" }) => {
+  let text = "";
+
+  switch (status) {
+    case "Success":
+      text = "Nice!";
+      break;
+    case "Failure":
+      text = "Retry";
+      break;
+    default:
+      text = label;
+  }
+
+  return (
+    <StyledSubmitButton status={status} disabled={status !== "Active"}>
+      {status === "Loading" ? <Spinner size="16" /> : text}
+    </StyledSubmitButton>
+  );
 };
 
 export { SubmitButton };
--- src/components/Buttons/styles.js
+++ src/components/Buttons/styles.js
@@ -3,10 +3,22 @@ import styled from "styled-components";
 const StyledSubmitButton = styled.button`
   padding: 8px 72px;
   font-size: var(--fs-h600);
-  max-width: 200px;
+  transition: width 500ms linear;
+  width: ${({ status }) => (status === "Active" ? "200px" : "250px")};
   border: none;
   color: var(--n10);
-  background-color: var(--g200);
+  background-color: ${({ status }) => {
+    switch (status) {
+      case "Success":
+        return "var(--t200)";
+      case "Failure":
+        return "var(--r500)";
+      case "Loading":
+        return "var(--n40)";
+      default:
+        return "var(--g200)";
+    }
+  }};
 `;
 
 export { StyledSubmitButton };
