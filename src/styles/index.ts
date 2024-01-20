dfb79523e89de4e28b8bdfc6eab66d2e3d649135
wfercanas <fernando.canas@outlook.com>
2024-01-19 23:27:13 -0500

    feat: basic global styles

---
+++ src/styles/index.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: "Noto Sans", "Arial";
+    padding: 32px 80px;
+  }
+`;
+export { GlobalStyle };
