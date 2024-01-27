241a7bbf8d28e5c4609b98e52b18e0552659ad93
wfercanas <fernando.canas@outlook.com>
2024-01-27 18:52:36 -0500

    feat: basic rendering of article

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,7 +1,20 @@
 import Markdown from "markdown-to-jsx";
+import { useLoaderData, useParams } from "react-router-dom";
+import { IArticle } from "../../types/article";
 
 function Article() {
-  return <Markdown>{"Hello World"}</Markdown>;
+  const { slug } = useParams();
+  const data = useLoaderData() as IArticle[];
+  const article = data.find((article) => article.slug === slug);
+
+  return article ? (
+    <>
+      <Markdown>{`# ${article.title}`}</Markdown>
+      <Markdown>{article.content}</Markdown>
+    </>
+  ) : (
+    <p>No article</p>
+  );
 }
 
 export { Article };
