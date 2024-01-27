2eb3bc964731764d1a29701d159f0fd52521badb
wfercanas <fernando.canas@outlook.com>
2024-01-27 18:51:54 -0500

    feat: getArticles ordered desc importing supabase client

--- src/loaders/getArticles.ts
+++ src/loaders/getArticles.ts
@@ -1,12 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "./supabase";
 
-const supabase = createClient(
-  "https://cxckwdonskgsdldtjwlv.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4Y2t3ZG9uc2tnc2RsZHRqd2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4MDYyNDcsImV4cCI6MjAyMTM4MjI0N30.hdNDFsnUgIuVvu-VcKNJLD_JkPIXmyOdCs-DIxJSzE4",
-);
-
-export async function getArticles() {
-  const { data } = await supabase.from("articles").select();
+async function getArticles() {
+  const { data } = await supabase
+    .from("articles")
+    .select("*")
+    .order("published_at", { ascending: false });
   console.log(data);
   return data;
 }
+
+export { getArticles };
