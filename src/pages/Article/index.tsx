599565bc51b53fca4ad251b0fac5b50c03a60a0f
wfercanas <fernando.canas@outlook.com>
2024-01-27 20:53:13 -0500

    feat: control width of article to be max 900px

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,6 +1,7 @@
 import Markdown from "markdown-to-jsx";
 import { useLoaderData, useParams } from "react-router-dom";
 import { IArticle } from "../../types/article";
+import { StyledArticle } from "./styles";
 
 function Article() {
   const { slug } = useParams();
@@ -8,10 +9,10 @@ function Article() {
   const article = data.find((article) => article.slug === slug);
 
   return article ? (
-    <>
+    <StyledArticle>
       <Markdown>{`# ${article.title}`}</Markdown>
       <Markdown>{article.content}</Markdown>
-    </>
+    </StyledArticle>
   ) : (
     <p>No article</p>
   );
---
+++ src/pages/Article/styles.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+const StyledArticle = styled.article`
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+export { StyledArticle };
